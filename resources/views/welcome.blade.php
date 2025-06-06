c65babb73fb5f8a19c7a30868e3a57c4940d857d
luqni <mluqnibaehaqi@gmail.com>
2025-06-06 17:21:34 +0700

    feat: update button register on hero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,7 +97,7 @@
           <p data-aos="fade-up" data-aos-delay="100">Quickly start your project now and set the stage for success<br></p>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
             <a href="{{ route('login') }}" class="btn-get-started">Login</a>
-            <a href="{{ route('register') }}" class="glightbox btn-watch-video d-flex align-items-center">Register</a>
+            <a href="{{ route('register') }}" class="btn">Register</a>
           </div>
           <img src="{{ asset('wp/assets/img/hero-services-img.webp') }}" class="img-fluid hero-img" alt="" data-aos="zoom-out" data-aos-delay="300">
         </div>
